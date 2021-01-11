54e2c16f64d11e9f2437aba7dd4732238f56dd9d
nyabe <nyabe.tabya@gmail.com>
2021-01-11 16:33:43 -0600

    Create project in Studio for testing sdk

---
+++ SDKNyabe/src/main/java/com/nyabe/sdknyabe/NyabeSampleClass.java
@@ -0,0 +1,12 @@
+package com.nyabe.sdknyabe;
+
+import android.util.Log;
+
+public class NyabeSampleClass {
+
+    private static final String TAG = "SUPER_NYABE_SDK";
+
+    public static void nDebug(String msg){
+        Log.d(TAG, msg);
+    }
+}
